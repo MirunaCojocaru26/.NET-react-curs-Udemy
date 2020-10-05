add3df24a869f463b324c44095288cf57e31940a
MirunaCojocaru26 <mirunacojocaru26@yahoo.com>
2020-10-05 11:02:21 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -2,15 +2,15 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using Persistence;
 using System.Threading.Tasks;
+using Persistence;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API
 {
