acbe373e8d18c53e06d3394da874ddceb3ca3de2
Miruna Cojocaru <miruna.cojocaru@totalsoft.ro>
2020-10-06 16:36:44 +0300

    End of section 5

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, Fragment} from 'react';
-import { Container, Header, Icon, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import axios from 'axios';
 import 'semantic-ui-css/semantic.min.css';
 import {IActivity} from '../models/activity';
@@ -8,17 +8,60 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 
 const App =() => {
   const[activities, setActivities] = useState<IActivity[]>([])
+  const[selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+  const[editMode, setEditMode] = useState(false);
+
+  const handleSelectActivity = (id:string) =>{
+    setSelectedActivity(activities.filter(a=> a.id === id)[0])
+    setEditMode(false);
+  }
+
+  const handleOpenCreateForm =() => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  } 
+
+  const handleCreateActivity = (activity: IActivity) =>{
+    setActivities([...activities,activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([...activities.filter(a => a.id !==activity.id), activity])
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handelDeleteActivity = (id:string) =>{
+    setActivities([...activities.filter(a=> a.id !== id)]);
+  }
+
   useEffect(() =>{
     axios.get<IActivity[]>('http://localhost:5000/api/activities').then((response) => {
+      let activities:IActivity[] =[];
+      response.data.forEach(activity => {
+        activity.date = activity.date.split('.')[0]
+        activities.push(activity)
+      })
       setActivities(response.data)
     });
   },[]);
 
     return (
       <Fragment>
-        <NavBar/>
+        <NavBar openCreateForm = {handleOpenCreateForm} />
         <Container style = {{marginTop: ' 7em'}}>
-          <ActivityDashboard activities ={activities}/>
+          <ActivityDashboard 
+            activities ={activities} 
+            selectActivity = {handleSelectActivity} 
+            selectedActivity = {selectedActivity!}
+            editMode = {editMode}
+            setEditMode = {setEditMode}
+            setSelectedActivity = {setSelectedActivity} 
+            createActivity = {handleCreateActivity}
+            editActivity = {handleEditActivity}
+            deleteActivity = {handelDeleteActivity}/>
         </Container>
       </Fragment>
     );
