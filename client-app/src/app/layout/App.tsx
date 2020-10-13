a721e5f47efa917e32a2a6977365c997ecd843e8
Miruna Cojocaru <miruna.cojocaru@totalsoft.ro>
2020-10-13 10:41:27 +0300

    save

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,6 +8,7 @@ import { Route, RouteComponentProps, Switch, withRouter} from 'react-router-dom'
 import HomePage from '../../features/home/HomePage'
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import LoginForm from '../../features/user/LoginForm'
 import NotFound from './NotFound';
 import {ToastContainer} from 'react-toastify'
 
@@ -25,6 +26,7 @@ const App: React.FC<RouteComponentProps> = ({location}) => {
             <Route exact path='/activities' component={ActivityDashboard} /> 
             <Route path='/activities/:id' component={ActivityDetails} /> 
             <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} /> 
+            <Route path='/login' component={LoginForm} />
             <Route component={NotFound}/>
             </Switch>      
           </Container>
