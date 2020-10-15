1c3632ef141732e8d28c690b99da46d2b71e3fba
Miruna Cojocaru <miruna.cojocaru@totalsoft.ro>
2020-10-15 17:58:11 +0300

    end of the day. 279

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,32 +1,55 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
+using AutoMapper;
 using Domain;
-using System;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Persistence;
-using AutoMapper;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query :IRequest<List<ActivityDto>>{}
-        public class Handler :IRequestHandler<Query, List<ActivityDto>>
+        public class ActivitiesEnvelope
+        {
+            public List<ActivityDto> Activities { get; set; }
+            public int ActivityCount { get; set; }
+        }
+        public class Query : IRequest<ActivitiesEnvelope>
+        {
+            public Query(int? limit, int? offset)
+            {
+                Limit = limit;
+                Offset = offset;
+            }
+            public int? Limit { get; set; }
+            public int? Offset { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, ActivitiesEnvelope>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
-            public Handler (DataContext context, IMapper mapper)
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
-                _context = context;
                 _mapper = mapper;
+                _context = context;
             }
-            public async Task<List<ActivityDto>> Handle(Query request, CancellationToken cancellation)
+
+            public async Task<ActivitiesEnvelope> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activities = await _context.Activities.ToListAsync();
-                return _mapper.Map<List<Activity>,List<ActivityDto>>(activities);
+                var queryable = _context.Activities.AsQueryable();
+                var activities = await queryable.Skip(request.Offset ?? 0).Take(request.Limit ?? 3)
+                    .ToListAsync();
+                return new ActivitiesEnvelope
+                {
+                    Activities = _mapper.Map<List<Activity>, List<ActivityDto>>(activities),
+                    ActivityCount = queryable.Count()
+                };
             }
         }
     }
